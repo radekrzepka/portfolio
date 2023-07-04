71a05c48492db43f050ebb17e83f2d6f693e504c
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-07-04 13:23:05 +0200

    changing description

--- frontend/src/components/Main/AboutMe/AboutMe.tsx
+++ frontend/src/components/Main/AboutMe/AboutMe.tsx
@@ -11,12 +11,16 @@ const AboutMe = () => {
 			</p>
 			<p className="mb-3">Jesem Radek i jestem front-end developerem.</p>
 			<p className="mb-3">
-				Specjalizuje się w tworzeniu stron i aplikacji internetowych. W
+				Interesuje się w tworzeniem stron i aplikacji internetowych. W
 				przyszłości chciałbym rozwinąć swoje umijętności w kierunku back-endu,
 				aby zostać full-stack developerem.
 			</p>
+			<p className="mb-3">
+				Zapraszam do zapoznania się z moimi projektami i skontaktowania się ze
+				mną, jeśli jesteś zainteresowany współpracą.
+			</p>
 			<h4 className="my-8 text-xl text-whiteHeadersColor">
-				Technologie i narzędzia z któych korzystam:
+				Technologie i narzędzia:
 			</h4>
 			<Technologies></Technologies>
 		</div>
